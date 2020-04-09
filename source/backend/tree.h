be9c422529d0bec5df9ec4ca7e2869fc231bbfd9
mandelshtamd <mandelstudy@gmail.com>
2020-04-09 20:33:31 +0300

    added basic directories for project

---
+++ source/backend/tree.h
@@ -0,0 +1,10 @@
+#ifndef FAMILY_TREE_TREE_H
+#define FAMILY_TREE_TREE_H
+
+
+class tree {
+
+};
+
+
+#endif //FAMILY_TREE_TREE_H
---
+++ source/main.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+#include "backend/tree.h"
+
+
+int main() {
+    std::cout << "Hello world!" << std::endl;
+}
