557ab0eca14efc87a93965469c1544522d7ffe42
mandelshtamd <mandelstudy@gmail.com>
2020-04-18 21:00:06 +0300

    added simple user interface

--- source/main.py
+++ source/main.py
@@ -1,1 +1,30 @@
-print("Hello, world!")
+import sys
+from PyQt5.QtWidgets import QWidget, QDesktopWidget, QApplication
+
+
+class Example(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.initUI()
+
+
+    def initUI(self):
+        self.resize(500, 500)
+        self.center()
+
+        self.setWindowTitle('Family tree')
+        self.show()
+
+
+    def center(self):
+        qr = self.frameGeometry()
+        cp = QDesktopWidget().availableGeometry().center()
+        qr.moveCenter(cp)
+
+        self.move(qr.topLeft())
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = Example()
+    sys.exit(app.exec_())
