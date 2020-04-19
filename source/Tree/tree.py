62fc6cd26d7f7e9c5832629f2aeece4cf650041d
margarita0303 <lashina.2000@gmail.com>
2020-04-19 14:50:56 +0300

    + classes Data, Node, Tree

--- source/Tree/tree.py
+++ source/Tree/tree.py
@@ -1,1 +1,15 @@
-print("Hello world!")
+class Data:
+    def __init__(self):
+        """Data constructor"""
+
+
+class Node(object):
+    def __init__(self, parent=None, data=None):
+        self.parent = parent
+        self.children = []
+        self.data = data
+
+
+class Tree(object):
+    def __init__(self, root=None):
+        self.root = root
