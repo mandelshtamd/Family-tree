dafdaf441131bc544dd4dd25790d68065092c009
mandelshtamd <mandelstudy@gmail.com>
2020-05-09 21:24:40 +0300

    update

--- source/main.py
+++ source/main.py
@@ -1,9 +1,13 @@
 import sys
 from PyQt5 import QtCore, QtGui, QtWidgets
-from PyQt5.QtWidgets import QApplication, QMainWindow, QWidget, QPushButton, QToolTip, QDesktopWidget
-from PyQt5.QtGui import QFont
+from PyQt5.QtCore import Qt, QPointF
+from PyQt5.QtWidgets import QApplication, QMainWindow, QWidget, QPushButton, QToolTip, QDesktopWidget, \
+    QGraphicsEllipseItem, QGraphicsItem, QGraphicsScene, QLineEdit, QInputDialog, QFormLayout, QFrame, QLabel, QDialog, \
+    QGraphicsLineItem, QGraphicsPathItem
+from PyQt5.QtGui import QFont, QPen, QPainter
 
-class Ui_MainWindow(object):
+
+class Ui_MainWindow(QWidget):
     def setupUi(self, MainWindow):
         MainWindow.setObjectName("MainWindow")
         MainWindow.setFixedSize(1585, 861)
@@ -16,6 +20,13 @@ class Ui_MainWindow(object):
         font.setPointSize(14)
         font.setBold(True)
         font.setWeight(75)
+
+        self.graphicsView = QtWidgets.QGraphicsView(self.centralwidget)
+        self.graphicsView.setStyleSheet("background-image: url(./фон4.png)")    
+        self.graphicsView.setGeometry(QtCore.QRect(-29, 110, 1630, 871))
+        self.graphicsView.setObjectName("graphicsView")
+        self.QGraphicsScene = QGraphicsScene()
+
         self.label.setFont(font)
         self.label.setText("")
         self.label.setPixmap(QtGui.QPixmap("./фон4.png"))
@@ -157,15 +168,24 @@ class Ui_MainWindow(object):
         self.label_8 = QtWidgets.QLabel(self.centralwidget)
         self.label_8.setGeometry(QtCore.QRect(690, 300, 151, 151))
         self.label_8.setText("")
-        self.label_8.setPixmap(QtGui.QPixmap("user2.png"))
+        self.label_8.setPixmap(QtGui.QPixmap("./user2.png"))
         self.label_8.setObjectName("label_8")
-        self.pushButton_11 = QtWidgets.QPushButton(self.centralwidget)
-        self.pushButton_11.setGeometry(QtCore.QRect(810, 300, 31, 31))
+        self.addPerson = QtWidgets.QPushButton(self.centralwidget)
+        self.addPerson.setGeometry(QtCore.QRect(810, 300, 31, 31))
+        self.addPerson.clicked.connect(self.toAddPerson)
+        self.addPerson.setEnabled(True)
+        self.addPerson.setObjectName("addPerson")
 
-        self.pushButton_11.setObjectName("pushButton_11")
-        self.pushButton_12 = QtWidgets.QPushButton(self.centralwidget)
-        self.pushButton_12.setGeometry(QtCore.QRect(690, 300, 31, 31))
-        self.pushButton_12.setObjectName("pushButton_12")
+
+
+        self.deletePerson = QtWidgets.QPushButton(self.centralwidget)
+        self.deletePerson.setGeometry(QtCore.QRect(690, 300, 31, 31))
+        self.deletePerson.setObjectName("deletePerson")
+        self.deletePerson.clicked.connect(self.toDeletePerson)
+        self.deletePerson.setEnabled(True)
+
+        self.nodesMap = []
+
         MainWindow.setCentralWidget(self.centralwidget)
         self.menubar = QtWidgets.QMenuBar(MainWindow)
         self.menubar.setGeometry(QtCore.QRect(0, 0, 1184, 19))
@@ -174,6 +194,56 @@ class Ui_MainWindow(object):
         self.statusbar = QtWidgets.QStatusBar(MainWindow)
         self.statusbar.setObjectName("statusbar")
         MainWindow.setStatusBar(self.statusbar)
+
+
+
+    def toAddPerson(self):
+        scene = self.QGraphicsScene
+        blackPen = QPen(Qt.black)
+        blackPen.setWidth(3)
+        circle = QGraphicsEllipseItem(10, 10, 100, 100)
+        circle.setFlag(QGraphicsItem.ItemIsMovable)
+
+        device = AddDevice()
+        circle.name = device.iText.text()
+        circle.bornYear = device.pText.text()
+        circle.status = device.uText.text()
+        circle.setToolTip(circle.name)
+        self.nodesMap.append(circle)
+
+        circle.setFlag(QGraphicsItem.isUnderMouse(circle))
+        scene.addItem(circle)
+        self.graphicsView.setScene(scene)
+        #self.rerender()
+
+
+    def toDeletePerson(self):
+        scene = self.QGraphicsScene
+        nameToDelete, ok = QInputDialog.getText(self, 'Delete person', 'Who you want to delete?:')
+        print(nameToDelete)
+        for node in self.nodesMap:
+            if (node.name == nameToDelete):
+                scene.removeItem(node)
+        self.graphicsView.setScene(scene)
+
+    def rerender(self):
+        scene = self.QGraphicsScene
+        for firstNode in self.nodesMap:
+            for secondNode in self.nodesMap:
+                line = QGraphicsLineItem(QtCore.QLineF(firstNode.pos(), secondNode.pos()))
+                line.setFlag(QGraphicsLineItem.ItemIsMovable)
+                scene.addItem(line)
+        self.graphicsView.setScene(scene)
+
+    # def paintEvent(self, QPaintEvent):
+    #     painter = QPainter()
+    #     painter.begin(self)
+    #     painter.setRenderHint(QPainter.Antialiasing)
+    #     painter.setPen(QtCore.Qt.red)
+    #     painter.setBrush(QtCore.Qt.white)
+    #     painter.drawLine(0, 0, 200, 200)
+
+
 
 
     def retranslateUi(self, MainWindow):
@@ -205,8 +275,8 @@ class Ui_MainWindow(object):
         self.pushButton_10.setText(_translate("MainWindow", "Tree search"))
         self.pushButton_10.setToolTip('Click <b>here</b> to quickly find the information you need in the tree')
         QToolTip.setFont(QFont('PurisaBold', 13))
-        self.pushButton_11.setText(_translate("MainWindow", "+"))
-        self.pushButton_12.setText(_translate("MainWindow", "-"))
+        self.addPerson.setText(_translate("MainWindow", "+"))
+        self.deletePerson.setText(_translate("MainWindow", "-"))
 
 
 
@@ -223,6 +293,47 @@ class mywindow(QtWidgets.QMainWindow):
         cp = QDesktopWidget().availableGeometry().center()
         qr.moveCenter(cp)
         self.move(qr.topLeft())
+
+
+class AddDevice(QDialog):
+    def __init__(self):
+        QDialog.__init__(self)
+        self.setWindowTitle("Person's information")
+        self.resize(400, 300)
+
+        iLabel = QLabel("Name and surname")
+        self.iText = QLineEdit()
+
+        uLabel = QLabel("Year of born")
+        self.uText = QLineEdit()
+
+        pLabel = QLabel("Status")
+        self.pText = QLineEdit()
+
+        okButton = QPushButton()
+        okButton.setText("OK")
+        okButton.clicked.connect(self.add)
+
+        frame = QFrame(self)
+
+        formLayout = QFormLayout(frame)
+        formLayout.addRow(iLabel, self.iText)
+        formLayout.addRow(uLabel, self.uText)
+        formLayout.addRow(pLabel, self.pText)
+        formLayout.addRow(okButton, okButton)
+
+        self.exec_()
+
+    def add(self):
+        name = self.iText.text()
+        yearOfBorn = self.uText.text()
+        status = self.pText.text()
+        if (name != ""):
+            self.close()
+
+
+
+
 
 if __name__ == '__main__':
     app = QtWidgets.QApplication([])
